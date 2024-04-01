858c6b2351d0b05688b70d0ccde1123541b60f2c
vladkrutskikh <kruts.media@gmail.com>
2024-03-31 18:49:10 -0600

    Send HW fixed

--- HW2 part 2.py
+++ HW2 part 2.py
@@ -15,15 +15,15 @@ driver.maximize_window()
 # open the url
 driver.get('https://www.target.com/')
 
-driver.find_element(By.XPATH, "//span[@class='styles__LinkText-sc-1e1g60c-3 dZfgoT h-margin-r-x3']").click()
+driver.find_element(By.XPATH, "//a[@data-test='@web/AccountLink']").click()
 
 sleep(6)
 
-driver.find_element(By.XPATH, "//span[@class='styles__ListItemText-sc-diphzn-1 jaMNVl' and text()='Sign in']").click()
+driver.find_element(By.XPATH, "//a[@data-test='accountNav-signIn']").click()
 
 sleep(6)
 
-text_shown = driver.find_element(By.XPATH, "//h1[@class='styles__StyledHeading-sc-1xmf98v-0 styles__AuthHeading-sc-kz6dq2-2 jhKFiw kcHdEa']").text
+text_shown = driver.find_element(By.XPATH, "//span[text()='Sign into your Target account']").text
 
 sleep(6)
 
