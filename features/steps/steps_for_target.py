646c3f37fc4d0c783a4abe6aa34e5408a66ebdcb
vladkrutskikh <kruts.media@gmail.com>
2024-05-13 15:17:27 -0600

    Added spaces among defs

--- features/steps/steps_for_target.py
+++ features/steps/steps_for_target.py
@@ -2,15 +2,18 @@ from selenium.webdriver.common.by import By
 from behave import given, when, then
 from time import sleep
 
+
 @given('Open Target page')
 def open_target(context):
     context.driver.get('https://www.target.com/')
     sleep(6)
 
+
 @when('Click on Cart icon')
 def click_on_cart_icon(context):
     context.driver.find_element(By.XPATH, "//div[@data-test='@web/CartIcon']").click()
     sleep(6)
+
 
 @then("Verify 'Your cart is empty' message is shown")
 def verify_cart_is_empty(context):
@@ -18,15 +21,18 @@ def verify_cart_is_empty(context):
     expected_text = 'Your cart is empty'
     assert expected_text == actual_text, f'Expected {expected_text}, but got {actual_text}'
 
+
 @when("Click Sign In")
 def click_sign_in(context):
     context.driver.find_element(By.XPATH, "//a[@data-test='@web/AccountLink']").click()
     sleep(4)
 
+
 @when("From right side navigation menu, click Sign In")
 def click_sign_in(context):
     context.driver.find_element(By.XPATH, "//a[@data-test='accountNav-signIn']").click()
     sleep(4)
+
 
 @then("Verify Sign In form opened")
 def verify_sign_in_form_opened(context):
